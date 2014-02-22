2e9461f254894db591a8f1c511debcaa1a703935
zhuao <zhuao1051@gmail.com>
2014-02-22 14:48:22 +0800

    ignore gen file

--- Android/gen/R.java.d
+++
@@ -1,6 +0,0 @@
-/Users/zhuao/workspace/AndroidSketch/Android/gen/com/tw/sketch/R.java \
- : /Users/zhuao/workspace/AndroidSketch/Android/res/layout/edit_text.xml \
-/Users/zhuao/workspace/AndroidSketch/Android/res/layout/function_lists.xml \
-/Users/zhuao/workspace/AndroidSketch/Android/res/layout/main.xml \
-/Users/zhuao/workspace/AndroidSketch/Android/res/values/strings.xml \
-/Users/zhuao/workspace/AndroidSketch/Android/bin/AndroidManifest.xml \
