1c88fdf62940a6ce08294335132a603ff4397fb1
zhuao <zhuao1051@gmail.com>
2014-02-26 22:06:45 +0800

    [zhuao]Move audio recorder to an independent package.

--- Android/src/main/java/com/tw/sketch/activity/AudioRecorderActivity.java
+++ Android/src/main/java/com/tw/sketch/activity/AudioRecorderActivity.java
@@ -2,16 +2,51 @@ package com.tw.sketch.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import com.tw.sketch.R;
+import com.tw.sketch.audiorecorder.AudioRecorderService;
 
 public class AudioRecorderActivity extends Activity {
 
+
+
+    private AudioRecorderService audioRecorderService = new AudioRecorderService(null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.recorder);
+
+        Button recorderBtn = ((Button) findViewById(R.id.record_btn));
+        attachAudioRecorder(recorderBtn);
+
+    }
+
+    private void attachAudioRecorder(final Button audioRecordBtn) {
+        audioRecordBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(AudioRecorderActivity.this, "started", Toast.LENGTH_SHORT).show();
+                audioRecorderService.startRecording();
+                return false;
+            }
+        });
+
+        audioRecordBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    Toast.makeText(AudioRecorderActivity.this, "stopped", Toast.LENGTH_SHORT).show();
+                    audioRecorderService.stopRecording();
+                }
+                return false;
+            }
+        });
     }
 
 }
---
+++ Android/src/main/java/com/tw/sketch/audiorecorder/AudioRecorderService.java
@@ -0,0 +1,71 @@
+package com.tw.sketch.audiorecorder;
+
+import android.content.Context;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.widget.Toast;
+import com.tw.sketch.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AudioRecorderService {
+
+    private MediaRecorder mediaRecorder = null;
+    private static SimpleDateFormat AUDIO_FILE_TIME_STAMP = new SimpleDateFormat("yyyyMMddHHmmssS");
+    private Context context;
+
+    public AudioRecorderService(Context context) {
+        this.context = context;
+    }
+
+    public void startRecording() {
+        try {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile(getTempAudioFile(context));
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (IOException e) {
+            mediaRecorder.reset();
+            mediaRecorder.release();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void stopRecording() {
+        mediaRecorder.stop();
+        mediaRecorder.reset();
+        mediaRecorder.release();
+    }
+
+    public void release() {
+        //TODO: To check whether recoreder is released.
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();
+            mediaRecorder.release();
+        }
+    }
+
+    private String getTempAudioFile(Context context) throws IOException {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            String path = Environment.getExternalStorageDirectory().getAbsolutePath()
+                    + "/" + context.getResources().getString(R.string.app_name);
+            File projectFolder = new File(path);
+            if (!projectFolder.exists()) {
+                projectFolder.mkdir();
+            }
+            return projectFolder.getAbsolutePath() + "/" + buildAudioFileName();
+        }
+        Toast.makeText(context, "SD card is not ready.", Toast.LENGTH_SHORT).show();
+        throw new RuntimeException("External storage error");
+    }
+
+    private String buildAudioFileName() {
+        return AUDIO_FILE_TIME_STAMP.format(new Date()) + ".3gp";
+    }
+}
