5a995c067cce9d6b211bb9a2577cabe923839075
zhuao <zhuao1051@gmail.com>
2015-07-14 13:59:53 +0800

    [Ao] Add drawerlayout in M

--- Android/src/main/java/me/zhuao/android/sketch/MainActivity.java
+++ Android/src/main/java/me/zhuao/android/sketch/MainActivity.java
@@ -1,21 +1,49 @@
 package me.zhuao.android.sketch;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import me.zhuao.android.sketch.activity.AudioRecorderActivity;
 import me.zhuao.android.sketch.activity.EditTextActivity;
 import me.zhuao.android.sketch.activity.MomentsActivity;
 import me.zhuao.android.sketch.activity.SpinnerActivity;
 import me.zhuao.android.sketch.activity.StepIndicatorActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.navigation)
+    NavigationView navigationView;
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.function_lists);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        attacheNavigationListener();
+    }
+
+    private void attacheNavigationListener() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.navigation_spinner) {
+                    spinner(null);
+                }
+                return false;
+            }
+        });
     }
 
     public void spinner(View view) {
